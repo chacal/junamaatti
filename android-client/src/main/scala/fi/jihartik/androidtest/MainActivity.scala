b4c9ba4a5de34ba06efd35be80a74cfda97b299c
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2010-04-25 16:06:12 +0300

    Loads hardcoded timetables on start.

--- android-client/src/main/scala/fi/jihartik/androidtest/MainActivity.scala
+++ android-client/src/main/scala/fi/jihartik/androidtest/MainActivity.scala
@@ -1,11 +1,20 @@
 package fi.jihartik.androidtest
 
-import android.app.Activity
 import android.os.Bundle
+import android.app.{ProgressDialog, Activity}
 
 class MainActivity extends Activity with HttpUtils {
   override def onCreate(savedInstanceState: Bundle) = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
+
+    withProgress("Loading timetable...") {
+      httpGet("http://www.omatlahdot.fi/omatlahdot/web?stopid=E1058&command=quicksearch&view=mobile")
+    }
+  }
+
+  def withProgress(message: String)(work : => Unit) {
+    val progress = ProgressDialog.show(this, "", message, true)
+    AsyncTask(work, progress.dismiss)
   }
 }
